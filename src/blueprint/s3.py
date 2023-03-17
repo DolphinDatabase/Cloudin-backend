f52f66504ca32bfa1f724f8c7884ae82e661e414
Abraao <abraao.vscncls@gmail.com>
2023-03-17 20:51:42 -0300

    download s3

--- src/blueprint/s3.py
+++ src/blueprint/s3.py
@@ -29,7 +29,47 @@ s3 = boto3.client(
 #Define caminho para download
 FILE_PATH = "downloads/s3"
  
+@s3bp.route('/list')
+def list():
+    # Lista todos os objetos do bucket
+    response = s3.list_objects(Bucket=s3_bucket_name)
+    # Extrai os nomes dos objetos e os retorna
+    obj_names = [obj['Key'] for obj in response['Contents']]
+    return jsonify({'objects': obj_names})
 
+
+
+
+@s3bp.route('/download/<file_name>')
+def download_file(file_name):
+    try:    
+        if not os.path.exists(FILE_PATH):
+            os.makedirs(FILE_PATH)
+
+        local_folder_path = os.path.join(os.getcwd(), 'downloads', 's3')
+        local_file_name = file_name
+        # Define o caminho completo do arquivo local
+        local_file_path = os.path.join(local_folder_path, local_file_name)
+
+        # Mede o tempo de download do arquivo
+        start_time = time.time()
+
+        # Faz o download do arquivo do S3
+        s3.download_file(s3_bucket_name, file_name, local_file_path)
+
+         # Obtém o tamanho do arquivo e o tipo MIME
+        file_size = os.path.getsize(local_file_path)
+        
+        # Calcula o tempo de download
+        download_time = time.time() - start_time
+        
+        return jsonify({
+            'download_time': download_time,
+            'file_size': file_size
+        })
+
+    except Exception as e:
+        return str(e)
 
 @s3bp.route('/upload/<file_name>')
 def upload_file(file_name):
