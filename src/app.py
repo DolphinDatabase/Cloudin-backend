8242794742dee659c66656a75ce7061023dc0679
Victor Cavichioli <vivictoraraujo@hotmail.com>
2023-03-16 21:27:21 -0300

    Acrescente funções de download
    Adicione funções de download, com autenticação do google drive e exponha endpoint para ser utilizado posteriormente pelos artefatos Front-end
    
    Jira: CLD-22 função Download de arquivo do Google drive

--- src/app.py
+++ src/app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
+import requests
+from .utils import google_drive
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def helloWorld():
     return "Hello World!"
+
+
+@app.route('/auth/google')
+def authenticate():
+    return google_drive.authenticate()
+    
+
+@app.route('/auth/google/refresh-token')
+def refresh_token():
+    return google_drive.refresh_token()
+
+
+@app.route('/google/files')
+def list_files(access_token=''):
+    return google_drive.list_files(access_token)
+
+
+@app.route('/google/download')
+def download_item(access_token='', file_id=''):
+ 
+    return google_drive.download_file(access_token, file_id)
