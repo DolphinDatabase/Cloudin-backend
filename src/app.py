e3b97681948e6f423aebdca5bdf91e7207d3c9c4
Pedro Motta <pedromotta321r@gmail.com>
2023-03-17 22:18:33 -0300

    add transaction routes

--- src/app.py
+++ src/app.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from .model.database import db 
+from .schema.schema import ma
+
+from .model.transaction import Transaction
+from .model.file import File
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://backend:api5sem@ec2-100-26-31-5.compute-1.amazonaws.com:3306/cloudin'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://backend:api5sem@ec2-54-227-6-234.compute-1.amazonaws.com:3306/cloudin'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 with app.app_context():
     db.drop_all()
     db.create_all()
+
+ma.init_app(app)
 
 @app.route("/")
 def helloWorld():
