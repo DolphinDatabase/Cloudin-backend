c188744238b5bd7cf8e85873e02ca6de920162c3
Victor Cavichioli <vivictoraraujo@hotmail.com>
2023-04-07 20:00:37 -0300

    #CLD-76 Introduza padrão de retorno

--- src/blueprint/google.py
+++ src/blueprint/google.py
@@ -1,19 +1,20 @@
 from http.client import HTTPException
-from flask import Blueprint,jsonify,request
+from flask import Blueprint, jsonify, request, make_response
 import requests
 import time
 import os
 import mimetypes
 import json
 from io import BytesIO
-#from ..utils.google_drive import google_drive
 
-drivebp = Blueprint('google', __name__, url_prefix='/google')
+# from ..utils.google_drive import google_drive
+
+drivebp = Blueprint("google", __name__, url_prefix="/google")
 
 # @drivebp.route('/auth')
 # def authenticate():
 #     return google_drive.authenticate()
-    
+
 
 # @drivebp.route('/auth/refresh-token')
 # def refresh_token():
@@ -24,69 +25,87 @@ drivebp = Blueprint('google', __name__, url_prefix='/google')
 # def list_files(access_token=''):
 #     return google_drive.list_files(access_token)
 
-def getMymetype(url):
-    return mimetypes.MimeTypes().guess_type(url,strict=True)
 
-@drivebp.route('/list', strict_slashes=False)
+def getMymetype(url):
+    return mimetypes.MimeTypes().guess_type(url, strict=True)
+
+
+@drivebp.route("/list", strict_slashes=False)
 def list_files():
     try:
-        token = request.headers.get('token')
-        url = 'https://www.googleapis.com/drive/v3/files'
+        token = request.headers.get("token")
+        url = "https://www.googleapis.com/drive/v3/files"
         headers = {"Authorization": f"Bearer {token}"}
-        params = {'pageSize': 1000, 'fields': 'nextPageToken, files(id, name, size)'}
+        params = {"pageSize": 1000, "fields": "nextPageToken, files(id, name, size)"}
         files = []
         next_page_token = True
         while next_page_token:
             response = requests.get(url, headers=headers, params=params)
             json_response = response.json()
-            files.extend(json_response['files'])
-            next_page_token = json_response.get('nextPageToken', None) 
-            params['pageToken'] = next_page_token
-        return {'result': files}
+            files.extend(json_response["files"])
+            next_page_token = json_response.get("nextPageToken", None)
+            params["pageToken"] = next_page_token
+        return make_response(jsonify({"result": files}), 200)
     except Exception as e:
-        return {'error':f'list files error: {e}'}
+        return make_response(jsonify({"error": f"list files error: {e}"}), 500)
 
 
-@drivebp.route('/download', strict_slashes=False)
-def download_file(file_id,file_name,token):
+@drivebp.route("/download", strict_slashes=False)
+def download_file(file_id, file_name, token):
     try:
         file_url = f"https://www.googleapis.com/drive/v3/files/{file_id}?alt=media"
         start_time = time.time()
-        response = requests.get(file_url, headers={"Authorization": f"Bearer {token}"}, stream=True)
-        if not os.path.exists('./downloads/google'):
-            os.makedirs('./downloads/google')
+        response = requests.get(
+            file_url, headers={"Authorization": f"Bearer {token}"}, stream=True
+        )
+        if not os.path.exists("./downloads/google"):
+            os.makedirs("./downloads/google")
 
-        output_file = os.path.join('./downloads/google', file_name)
+        output_file = os.path.join("./downloads/google", file_name)
         total_time = None
-        with open(output_file, 'wb') as output:
+        with open(output_file, "wb") as output:
             for chunk in response.iter_content(chunk_size=1024):
                 if chunk:
                     output.write(chunk)
                     total_time = time.time() - start_time
         file_size = os.path.getsize(output_file)
-        return {'time':total_time,'size':file_size}
+        return make_response(
+            jsonify({"title": file_name, "time": total_time, "size": file_size}), 200
+        )
     except Exception as e:
-        return {'error':f'download error: {e}'}
+        return make_response(jsonify({"error": f"download error: {e}"}), 500)
 
 
-@drivebp.route('/upload', strict_slashes=False)
-def upload_file(file_name,token, origin):
+@drivebp.route("/upload", strict_slashes=False)
+def upload_file(file_name, token, origin):
     try:
         url = "https://www.googleapis.com/drive/v2/files"
         data = {
             "title": file_name,
-            "mimeType": getMymetype('./downloads/google/'+file_name)[0],
-            "description": "Powered by Cloud-in"
+            "mimeType": getMymetype("./downloads/google/" + file_name)[0],
+            "description": "Powered by Cloud-in",
         }
+        output_file = os.path.join("./downloads/google", file_name)
         start_time = time.time()
-        req = requests.post(url,headers={"Authorization": f"Bearer {token}"},data=json.dumps(data))
-        file_id= req.json()['selfLink'].split("/")[-1]
-        req_content = "https://www.googleapis.com/upload/drive/v2/files/"+file_id+"?uploadType=media"
-        with open('./downloads/'+origin+'/'+file_name,'rb') as file:
+        req = requests.post(
+            url, headers={"Authorization": f"Bearer {token}"}, data=json.dumps(data)
+        )
+        file_id = req.json()["selfLink"].split("/")[-1]
+        req_content = (
+            "https://www.googleapis.com/upload/drive/v2/files/"
+            + file_id
+            + "?uploadType=media"
+        )
+        with open("./downloads/" + origin + "/" + file_name, "rb") as file:
             content = BytesIO(file.read())
-        req = requests.put(req_content,headers={"Authorization": f"Bearer {token}"},data=content)
+        req = requests.put(
+            req_content, headers={"Authorization": f"Bearer {token}"}, data=content
+        )
         total_time = time.time() - start_time
-        os.remove('./downloads/'+origin+'/'+file_name)
-        return{'time':total_time}
-    except Exception as error:
-        return {'error':f'upload error: {error}'}
+        file_size = os.path.getsize(output_file)
+        os.remove("./downloads/" + origin + "/" + file_name)
+        return make_response(
+            jsonify({"title": file_name, "time": total_time, "size": file_size}), 200
+        )
+    except Exception as e:
+        return make_response(jsonify({"error": f"upload error: {e}"}), 500)
--- src/blueprint/transaction.py
+++ src/blueprint/transaction.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, make_response
 from ..model.database import db
 from ..schema.transactionSchema import TransactionSchema
 import importlib.util
@@ -6,77 +6,79 @@ import importlib.util
 from ..model.transaction import Transaction
 from ..model.file import File
 
-tbp = Blueprint('transaction', __name__, url_prefix="/transaction")
+tbp = Blueprint("transaction", __name__, url_prefix="/transaction")
 
-@tbp.route('/<id>', methods=['GET'], strict_slashes=False)
+
+@tbp.route("/<id>", methods=["GET"], strict_slashes=False)
 def list_transaction(id):
     schema = TransactionSchema(many=True)
     query = Transaction.query.filter_by(application=id).all()
     transactions = schema.dump(query)
-    return jsonify(transactions)
+    response_body = jsonify(transactions)
+    return make_response(response_body, 200)
 
-@tbp.route('/', methods=['POST'], strict_slashes=False)
+
+@tbp.route("/", methods=["POST"], strict_slashes=False)
 def create_transaction():
     body = request.get_json()
-    origin_token = request.headers.get('origin_token')
-    destiny_token = request.headers.get('destiny_token')
-    application = request.headers.get('application')
+    origin_token = request.headers.get("origin_token")
+    destiny_token = request.headers.get("destiny_token")
+    application = request.headers.get("application")
     transaction_data = {}
-    
+
     transaction = Transaction()
-    transaction.origin = body['origin']
-    transaction.destiny = body['destiny']
+    transaction.origin = body["origin"]
+    transaction.destiny = body["destiny"]
     transaction.status = "Em andamento"
     transaction.application = application
-    
+
     db.session.add(transaction)
     db.session.commit()
 
     concluido = True
-    #execute download
-    origin = importlib.util.spec_from_file_location(body['origin'],"./src/blueprint/"+body['origin']+".py").loader.load_module()
-    download = getattr(origin,"download_file")
-    for f in body['files']:
-        metadata_download = download(f['file_id'],f['file_name'],origin_token)
-        if metadata_download.get('time'):
-            transaction_data[f['file_name']]={'time':metadata_download['time'],'size':metadata_download['size']}
+    # execute download
+    origin = importlib.util.spec_from_file_location(
+        body["origin"], "./src/blueprint/" + body["origin"] + ".py"
+    ).loader.load_module()
+    download = getattr(origin, "download_file")
+    for f in body["files"]:
+        metadata_download = download(f["file_id"], f["file_name"], origin_token)
+        if metadata_download.get("time"):
+            transaction_data[f["file_name"]] = {
+                "time": metadata_download["time"],
+                "size": metadata_download["size"],
+            }
         else:
-            print("webhook 1")
-            concluido = False
-            transaction_data = {f['file_name']:metadata_download}
-            break
+            message = f"Erro ao realizar o download do arquivo {f['file_name']}."
+            return make_response(jsonify({"message": message}), 400)
 
-    #execute upload
+    # execute upload
     if concluido:
-        destiny = importlib.util.spec_from_file_location(body['destiny'],"./src/blueprint/"+body['destiny']+".py").loader.load_module()
-        upload = getattr(destiny,"upload_file")
-        for f in body['files']:
-            metadata_upload = upload(f['file_name'],destiny_token,body['origin'])
-            if metadata_upload.get('time'):
-                transaction_data[f['file_name']]['time']+= metadata_upload['time']
+        destiny = importlib.util.spec_from_file_location(
+            body["destiny"], "./src/blueprint/" + body["destiny"] + ".py"
+        ).loader.load_module()
+        upload = getattr(destiny, "upload_file")
+        for f in body["files"]:
+            metadata_upload = upload(f["file_name"], destiny_token, body["origin"])
+            if metadata_upload.get("time"):
+                transaction_data[f["file_name"]]["time"] += metadata_upload["time"]
             else:
-                print("webhook")
-                concluido = False
-                transaction_data = {f['file_name']:metadata_upload}
-                break
+                message = f"Erro ao realizar o upload do arquivo {f['file_name']}."
+                return make_response(jsonify({"message": message}), 400)
 
-    
-    #save in db
+    # save in db
     if concluido:
         transaction.status = "Concluido"
         for f in transaction_data:
             file = File()
             file.name = f
-            file.time = transaction_data[f]['time']
-            file.size = transaction_data[f]['size']
+            file.time = transaction_data[f]["time"]
+            file.size = transaction_data[f]["size"]
             transaction.file.append(file)
             db.session.add(file)
     else:
         transaction.status = "Erro"
-    
+
     db.session.add(transaction)
     db.session.commit()
-    return jsonify(transaction_data)
-
-
-        
+    return make_response(jsonify(transaction_data))
