51165597f75491e3357e91a5bfe50f551106ff3c
Beatriz Medeiros <biamedeirosassim@gmail.com>
2023-05-12 19:13:56 -0300

    unit test s3

--- test/unit-test/test_s3.py
+++ test/unit-test/test_s3.py
@@ -1,12 +1,24 @@
 import os
 import sys
-
+import pytest
 from unittest.mock import MagicMock,patch
 from src.services.s3 import s3Service
+from src.app import app, db
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-def test_filesByFolder():
+@pytest.fixture(scope="session")
+def init_database():
+    engine = create_engine("sqlite:///:memory:")
+    db.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    yield session
+    session.rollback()
+
+def test_files_by_folder():
     boto3_client = MagicMock(name= 'boto3.client')
     boto3_client.list_objects.return_value = {
         'Contents':["folder","file1","file2"]
@@ -16,7 +28,7 @@ def test_filesByFolder():
         result = service.files_by_folder(None)
     assert result == 2
 
-def test_listFilesByFolder():
+def test_list_files_by_folder():
     boto3_client = MagicMock(name= 'boto3.client')
     boto3_client.list_objects.return_value = {
         'Contents': [
@@ -34,22 +46,22 @@ def test_listFilesByFolder():
         {'id': 'folder/subfolder/file3.txt', 'name': 'subfolder'}
     ]
 
-
-
-# @mock.patch('boto3.client')
-# def test_list_folders_valid_token(mock_get):
-#     mock_get.list_objects.return_value = { 
-#         "CommonPrefixes": [
-#             {"Prefix":"teste1/"},
-#             {"Prefix":"teste2/"},
-#         ]
-#     }
-#     client = app.test_client()
-#     response = client.get("/s3/list/folder", headers={"token":"teste teste teste teste"})
-#     assert response.status_code == 200
-#     assert response.json != {
-#         "result": [
-#             {"id":"teste1", "name":"teste1"},
-#             {"id":"teste2", "name":"teste2"},
-#         ]
-#     }
+@pytest.fixture(scope="session")
+def test_list_folders_valid_token():
+    boto3_client = MagicMock(name= 'boto3.client')
+    boto3_client.list_objects.return_value = { 
+        "CommonPrefixes": [
+            {"Prefix":"teste1/"},
+            {"Prefix":"teste2/"},
+        ]
+    }
+    with patch('boto3.client', return_value=boto3_client):
+        client = app.test_client()
+        result = client.get("/s3/list/folder", headers={"token":"token token token token"})
+    assert result.status_code == 200
+    assert result.json == {
+        "result": [
+            {"id":"teste1", "name":"teste1"},
+            {"id":"teste2", "name":"teste2"},
+        ]
+    }
