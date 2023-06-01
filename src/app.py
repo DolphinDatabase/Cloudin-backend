44fbe553bbaa86afaed3dbde706feffb5e90c28a
Victor Cavichioli <vivictoraraujo@hotmail.com>
2023-06-01 12:57:34 -0300

    Remove tgz and add url config

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ from dotenv import set_key
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///:memory:")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 CORS(app)
 warnings.filterwarnings(
